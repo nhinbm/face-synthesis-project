2e3513b5a47395b6371bfca0c821b681a4ce2653
thuanphat03 <144221446+thuanphat03@users.noreply.github.com>
2024-03-13 21:36:59 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from st_pages import Page, show_pages
-
-show_pages(
-    [
-        Page("deepfake_page.py", "Deepfake", "🏠"),
-        Page("expression_page.py", "Expression", ":books:"),
-    ]
-)
